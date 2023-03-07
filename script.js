dc0c06e4c4baab65e078d6d55d7089bef8ce4d78
SAKET SRIVASTAVA <127039760+SAKET687@users.noreply.github.com>
2023-03-07 21:23:07 +0530

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ alert ("Invalid User Id/Password");
 attempt -- ;
 alert("Only "+attempt+" attempt(s) are left");
         if(attempt==0){
-            alert("You have reached maximum number of attempts.")
+            alert("You have reached maximum number of attempts.");
             document.getElementById("username").disabled = true;
             document.getElementById("password").disabled = true;
             document.getElementById("submit").disabled = true;
